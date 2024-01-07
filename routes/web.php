07a815189b14482fbd83c794d94813809faf3452
hammy134 <muhammad78600001@gmail.com>
2024-01-07 21:08:08 +0500

    Admin and user complete but some bugs occurs

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
+Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+require __DIR__.'/auth.php';
+
+// Admin
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
+    Route::namespace('Auth')->middleware('guest:admin')->group(function(){
+        // login route
+        Route::get('login','AuthenticatedSessionController@create')->name('login');
+        Route::post('login','AuthenticatedSessionController@store')->name('adminlogin');
+
+    });
+
+    Route::middleware('admin')->group(function(){
+        Route::get('dashboard','HomeController@index')->name('dashboard');
+
+        Route::get('admin-test','HomeController@adminTest')->name('admintest');
+        Route::get('editor-test','HomeController@editorTest')->name('editortest');
+
+        Route::resource('posts','PostController');
+
+    });
+    Route::post('logout','Auth\AuthenticatedSessionController@destroy')->name('logout');
 });
 
 require __DIR__.'/auth.php';
